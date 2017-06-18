def31ef4a2aed15e9d59fae3a099a9b564c95a59
phuonghoang713 <phuonghoang713@gmail.com>
2017-06-19 06:27:45 +0700

    remove code junk in schema.rd and update string

--- db/schema.rb
+++ db/schema.rb
@@ -30,10 +30,5 @@ ActiveRecord::Schema.define(version: 20170617034042) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "sessions", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
